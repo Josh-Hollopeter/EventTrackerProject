7351f0a1567739d9b36987bb2b43a766eaf14d9b
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-02-15 15:24:47 -0700

    delete working

--- EventTracker/src/main/resources/static/script.js
+++ EventTracker/src/main/resources/static/script.js
@@ -2,121 +2,141 @@
  * 
  */
 
-getPlaygrounds = function(){
-//var xhr = new XMLHttpRequest(xhr);
-let xhr = new XMLHttpRequest();
-xhr.open('GET','api/playgrounds', true);
-console.log("in get playgrounds")
-xhr.onreadystatechange = function() {
-	console.log("in get playgrounds 2")
-	if (xhr.readyState === 4 && xhr.status < 400) {
-		var data = JSON.parse(xhr.responseText);
-		console.log("in get playgrounds 3")
-		console.log(data);
-		displayTables(data);
-	}
+getPlaygrounds = function() {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/playgrounds', true);
+	console.log("in get playgrounds")
+	xhr.onreadystatechange = function() {
+		console.log("in get playgrounds 2")
+		if (xhr.readyState === 4 && xhr.status < 400) {
+			var data = JSON.parse(xhr.responseText);
+			console.log("in get playgrounds 3")
+			console.log(data);
+			displayTables(data);
+		}
 
-	if (xhr.readyState === 4 && xhr.status >= 400) {
-		console.error(xhr.status + ': ' + xhr.responseText);
-		console.log("in error")
-	}
-};
-xhr.send(null);
+		if (xhr.readyState === 4 && xhr.status >= 400) {
+			console.error(xhr.status + ': ' + xhr.responseText);
+			console.log("in error")
+		}
+	};
+	xhr.send(null);
 }
-window.addEventListener('load', function(){
-	
+window.addEventListener('load', function() {
+
 	init();
 })
 
-var createPlayground = function(){
-console.log("in create")
-let xhr = new XMLHttpRequest();
-xhr.open('POST', 'api/playgrounds', true);
+var createPlayground = function() {
+	console.log("in create");
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/playgrounds', true);
 
-xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON request body
+	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON
+																// request body
 
-xhr.onreadystatechange = function() {
-	console.log("in create 3")
-  if (xhr.readyState === 4 ) {
-    if ( xhr.status == 200 || xhr.status == 201 ) { // Ok or Created
-      var data = JSON.parse(xhr.responseText);
-//      createPlayground(data);
-      console.log("meeeeow" + data)
-      console.log(data);
-    }
-    else {
-      console.log("POST request failed.");
-      console.error(xhr.status + ': ' + xhr.responseText);
-    }
-  }
-   // Convert JS object to JSON string
+	xhr.onreadystatechange = function() {
+		console.log("in create 3");
+		if (xhr.readyState === 4) {
+			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
+				var data = JSON.parse(xhr.responseText);
+				// createPlayground(data);
+				console.log("meeeeow" + data)
+				console.log(data);
+			} else {
+				console.log("POST request failed.");
+				console.error(xhr.status + ': ' + xhr.responseText);
+			}
+		}
 
+	};
+	let form = document.createPlayground;
+	console.log(form.name.value);
+	console.log(form.visited.value);
 
-// xhr.send(null)
-};
-let form = document.createPlayground;
-console.log(form.name.value);
-console.log(form.visited.value);
-//var stuff = form.visited.value.is(':checked');
-//console.log(stuff);
-//if(form.visited.value === 'on'){
-//	form.visited.value = true;
-//}else{
-//	form.visited.value = false;
-//}
-console.log(form.description.value);
-var playground = {
-		  name: form.name.value,
-		  visited: form.visited.value,
-		  location: form.location.value,
-//		  rating: 1,
-		  description : form.description.value
-//		  atmosphere : "",
-//		  image : ""
+	console.log(form.description.value);
+	var playground = {
+		name : form.name.value,
+		visited : form.visited.value,
+		location : form.location.value,
+		// rating: 1,
+		description : form.description.value
+	// atmosphere : "",
+	// image : ""
 
-		};
-var userObjectJson = JSON.stringify(playground);
-xhr.send(userObjectJson)
-console.log("create")
+	};
+	var userObjectJson = JSON.stringify(playground);
+	xhr.send(userObjectJson)
+	console.log("create")
+}
+function deletePlayground(id) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('DELETE', 'api/playgrounds/' + id , true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status < 400) {
+			init();
+			
+		}
+		if (xhr.readyState === 4 && xhr.status >= 400) {
+			console.error(xhr.status + ': ' + xhr.responseText);
+			
+		}
+	};
+
+	xhr.send(null);
 }
 
 function init() {
 
 	console.log("here in init")
-	
-	var button =  document.getElementById('create')
+
+	var button = document.getElementById('create')
 	button.addEventListener('click', function(e) {
 		event.preventDefault();
 		createPlayground();
-		
+
 		console.log("here in init 3")
 	})
+	let tableRemove;
+	if(tableRemove = document.getElementById("allPlaygroundTable")){
+	tableRemove.parentNode.removeChild(tableRemove)
+	}
 	getPlaygrounds();
-console.log("here in init 2")
+	console.log("here in init 2")
 }
 
-
-
-displayTables = function(data){
+displayTables = function(data) {
 	console.log("here in display");
 	let body = document.getElementsByTagName('body')[0];
 	let table = document.createElement('table');
+	table.id = "allPlaygroundTable";
 	body.appendChild(table);
 
-	var tableMaker = function(values,row){
+	var tableMaker = function(values, row) {
 
-		for(var i =0; i < values.length; i++){
+		for (var i = 0; i < values.length; i++) {
 			let td = document.createElement('td');
+
 			td.textContent = values[i];
+
 			row.appendChild(td);
 		}
-				
+
 	}
-	
-	for(var i =0; i < data.length;i ++){
+
+	for (var i = 0; i < data.length; i++) {
 		let values = Object.values(data[i]);
 		let row = document.createElement('tr')
+		let deleteButton = document.createElement('button');
+		deleteButton.appendChild(document.createTextNode('Delete'));
+		deleteButton.value = values[0];
+		deleteButton.innerHtml = "delete";
+		deleteButton.addEventListener("click", function() {
+			deletePlayground(deleteButton.value);
+		});
+		row.appendChild(deleteButton);
 		table.appendChild(row);
-		tableMaker(values,row);
+		tableMaker(values, row);
 	}
 }
